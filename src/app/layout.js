2e151228ceb0388a3ac19ef03712ac0f9757c775
TNC9 <koonbenzkub@hotmail.com>
2023-08-07 15:48:34 +0700

    Finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Thananchai Chaimanee"
+            studentId="630612101"
           />
         </div>
       </body>
